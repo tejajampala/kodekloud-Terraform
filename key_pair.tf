327248cb316049e6bb295a20ec0ab8b44a790d46
tejajampala <33386784+tejajampala@users.noreply.github.com>
2025-12-21 17:14:38 +1100

    key pair terraform

---
+++ key_pair.tf
@@ -0,0 +1,22 @@
+# Generate a private key
+resource "tls_private_key" "xfusion_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create Key Pair using the public key
+resource "aws_key_pair" "xfusion_kp" {
+  key_name   = "xfusion-kp"
+  public_key = tls_private_key.xfusion_key.public_key_openssh
+}
+
+# Save private key locally (optional but common)
+resource "local_file" "xfusion_private_key" {
+  content         = tls_private_key.xfusion_key.private_key_pem
+  filename        = "/home/bob/xfusion-kp.pem"
+  file_permission = "0400"
+}
+
+output "key_pair_name" {
+  value = aws_key_pair.xfusion_kp.key_name
+}
