3c8381b4b5b012edcdca7b38576c397f8e1ed2a6
tejajampala <33386784+tejajampala@users.noreply.github.com>
2025-12-24 18:12:17 +1100

    Create VPC resource with CIDR block and tags

---
+++ vpc_creation_cidr.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "devops_vpc" {
+  cidr_block           = "192.168.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "xfusion-vpc"
+  }
+}
